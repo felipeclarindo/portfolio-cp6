179d12e317602a177fe1f9fc2fe34475d4f94c1e
Caepena <caepenafiel@gmail.com>
2024-10-26 10:20:09 -0300

    Alteração dos valores do integrante Caetano, estilização adicional do Menu e correção de algumas coisas.

--- portifolio/src/app/api/integrantes/integrantes.ts
+++ portifolio/src/app/api/integrantes/integrantes.ts
@@ -13,9 +13,9 @@ export const integrantes: IntegrantesProps[] = [
     id: 3,
     nome: "Caetano",
     image:
-      "https://cdn.discordapp.com/attachments/1294144540143259659/1299638739923898410/075ba61d-6111-4197-bd9c-bc011749abff.png?ex=671dee65&is=671c9ce5&hm=01b9581f1c11f83e29e4a830a3df3357c5d87b78fb4a81a030376cbd5f84159d&",
-    github: "https://github.com/felipeclarindo",
-    linkedin: "https://linkedin.com/in/felipeclarindo",
+      "https://cdn.discordapp.com/attachments/473598054138904586/1299711794528653312/382446272_6545596672156558_922302855324028153_n.jpg?ex=671e326f&is=671ce0ef&hm=9cbdc235401ea7715d5980e626158d4fdb2a55048949c38e222e24f8d3506bdb&",
+    github: "https://github.com/Caepena",
+    linkedin: "https://linkedin.com/in/caetano-penafiel",
   },
   {
     id: 4,
@@ -27,7 +27,7 @@ export const integrantes: IntegrantesProps[] = [
   },
   {
     id: 5,
-    nome: "Kauan",
+    nome: "Kauã",
     image:
       "https://cdn.discordapp.com/attachments/1294144540143259659/1299638455915249684/aa1efaa4-9dfd-4765-8160-df7ded378c4f.png?ex=671dee22&is=671c9ca2&hm=953ee444ddfc8d06f2a2cad4af348f1b97e04b9b5ca0d411b1d2c422c9bdf634&",
     github: "https://github.com/felipeclarindo",
--- portifolio/src/components/Header/index.tsx
+++ portifolio/src/components/Header/index.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import Menu from "../Menu";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="h-[120px] flex justify-center items-center gap-2 bg-orange-50 text-black p-4">
-      <Image
-        className="absolute left-4"
-        src="/favicon.png"
-        alt="icone de uma mão"
-        width={75}
-        height={75}
-      />
-      <Menu />
+       <Link className="absolute left-4 cursor-pointer transition-all duration-300 ease-in-out hover:opacity-65" href="/">
+        <Image
+          src="/favicon.png"
+          alt="icone de uma mão"
+          width={75}
+          height={75}
+        />
+      </Link>
+      <Menu/>
     </header>
   );
 }
